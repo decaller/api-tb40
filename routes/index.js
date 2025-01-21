eeec31430768011c6fb9e864585e61c544157983
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-01-21 02:16:21 +0000

    serving static files on api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
+
+// Serve static files from the "public" directory
+router.use(express.static(path.join(__dirname, 'public')));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
