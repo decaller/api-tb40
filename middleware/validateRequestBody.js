15f574e8ab73c3e090a4aa466a64b30291139f97
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-01-21 03:38:49 +0000

    add 40 scores valdiation

--- middleware/validateRequestBody.js
+++ middleware/validateRequestBody.js
@@ -1,5 +1,5 @@
 function validateRequestBody(req, res, next) {
-//   console.log(req.body);
+  //   console.log(req.body);
   const { parts } = req.body;
 
   if (!parts || !parts.umum || !parts.tb40) {
@@ -15,15 +15,22 @@ function validateRequestBody(req, res, next) {
     !umum.lahir.tanggal ||
     !umum.tanggal
   ) {
-    return res.status(400).json({ error: "Invalid umum data" });
+    return res.status(400).json({ error: "Invalid minimum 'umum' data" });
   }
 
   if (
     !Array.isArray(tb40) ||
     tb40.length !== 40 ||
-    !tb40.every((score) => typeof score === "number")
+    !tb40.every(
+      (score) => typeof score === "number" && score >= 0 && score <= 100
+    )
   ) {
-    return res.status(400).json({ error: "Invalid tb40 data" });
+    return res
+      .status(400)
+      .json({
+        error:
+          "Invalid tb40 data, should be array of 40 data of number between 0-100",
+      });
   }
 
   next();
